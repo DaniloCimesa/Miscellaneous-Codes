0b359e56c355fefcf1aefe284f32ab19c60caf9a
Danilo Cimesa <102156507+DaniloCimesa@users.noreply.github.com>
2022-07-28 15:37:25 +0200

    Update Median.sql

--- Median.sql
+++ Median.sql
@@ -3,7 +3,7 @@
 There is no median function in SQL so you will have to create it yourself.
 Just find the lowest value of the top 50% of all values and highest value of bottom 50% of all values, then add values together and divide it by 2.*/
 
-```
+
 Select
 
 ((
@@ -16,4 +16,4 @@ select min(countryid)
 from tblcountry
 where countryid in (Select top 50 percent countryid from tblCountry order by CountryId desc)
 ))/2  as median
-```
+
