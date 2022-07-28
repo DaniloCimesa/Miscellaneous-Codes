6ac3fe5b630e28c3307d0adbd7b8bf6241164405
Danilo Cimesa <102156507+DaniloCimesa@users.noreply.github.com>
2022-07-28 15:36:56 +0200

    Update and rename Median.md to Median.sql

--- Median.sql
+++ Median.sql
@@ -1,10 +1,9 @@
-Median in SQL
+/*Median in SQL
 
 There is no median function in SQL so you will have to create it yourself.
-Just find the lowest value of the top 50% of all values and highest value of bottom 50% of all values, then add values together and divide it by 2.
+Just find the lowest value of the top 50% of all values and highest value of bottom 50% of all values, then add values together and divide it by 2.*/
 
 
-```
 Select
 
 ((
@@ -17,4 +16,4 @@ select min(countryid)
 from tblcountry
 where countryid in (Select top 50 percent countryid from tblCountry order by CountryId desc)
 ))/2  as median
-```
+
